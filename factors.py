bb36831e0e062270dc3bb08e8dc8001cf75f177f
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-11-26 11:04:33 -0800

    test

--- factors.py
+++ factors.py
@@ -3,24 +3,45 @@
 import sys
 import math
 
-def factorize_number(n):
-    """
-    Factorize a number into a product of two smaller numbers.
+import random
 
-    :param n: The number to factorize.
-    :return: A tuple (p, q) where p * q = n.
-    """
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return i, n // i
-    return n, 1
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def pollards_rho(n):
+    if n % 2 == 0:
+        return 2
+    x = random.randint(1, n-1)
+    y = x
+    c = random.randint(1, n-1)
+    d = 1
+    f = lambda x: (x**2 + c) % n
+    while d == 1:
+        x = f(x)
+        y = f(f(y))
+        d = gcd(abs(x - y), n)
+    return d
+
+def factorize_number(n):
+    if n == 1:
+        return 1, 1
+    if is_prime(n):
+        return n, 1
+
+    factor = pollards_rho(n)
+    return factor, n // factor
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 def factorize_file(input_file):
-    """
-    Factorize numbers from a file and print the factorization.
-
-    :param input_file: The file containing natural numbers to factor.
-    """
     with open(input_file, 'r') as file:
         for line in file:
             number = int(line.strip())
