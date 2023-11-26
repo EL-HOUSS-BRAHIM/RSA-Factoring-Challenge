4a88ac3371b542e4e3346b18276f0772466b462f
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-11-26 11:17:52 -0800

    test

--- factors.py
+++ factors.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python3
 
-# factors.py
-
-import sympy
 import sys
 
-def factorize(file_path):
-    with open(file_path, 'r') as file:
-        numbers = file.read().splitlines()
+def pollard_rho(n):
+    if n % 2 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
 
-    for num in numbers:
-        n = int(num)
-        factors = sympy.factorint(n)
-        factors_str = '*'.join([f"{factor}**{exponent}" for factor, exponent in factors.items()])
-        print(f"{num}={factors_str}")
+    x = 2
+    y = 2
+    d = 1
 
-if __name__ == "__main__":
+    while d == 1:
+        x = (x * x + 1) % n
+        y = (y * y + 1) % n
+        y = (y * y + 1) % n
+        d = gcd(abs(x - y), n)
+
+    return d
+
+def factorize(n):
+    if n == 1:
+        return []
+
+    factors = []
+    while n > 1:
+        p = pollard_rho(n)
+        factors.append(p)
+        n //= p
+
+    return factors
+
+def main():
     if len(sys.argv) != 2:
         print("Usage: factors <file>")
         sys.exit(1)
 
-    file_path = sys.argv[1]
-    factorize(file_path)
+    with open(sys.argv[1]) as f:
+        for line in f:
+            n = int(line)
+            factors = factorize(n)
+            print(f"{n} = {' * '.join(map(str, factors))}")
+
+if __name__ == "__main__":
+    main()
