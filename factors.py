0d56384ed2d8318e982bf4ad011a1358795aa4eb
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-11-26 11:11:15 -0800

    test

--- factors.py
+++ factors.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+# factors.py
+
 import sympy
 import sys
 import random
@@ -21,6 +24,16 @@ def pollard_rho(n):
 
     return d
 
+def trial_division(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            return i
+    return n
+
 def factorize(file_path):
     with open(file_path, 'r') as file:
         numbers = file.read().splitlines()
@@ -29,6 +42,13 @@ def factorize(file_path):
         n = int(num)
         factors = []
 
+        # Use trial division for small factors
+        while n > 1 and n < 1000000:
+            factor = trial_division(n)
+            factors.append(factor)
+            n //= factor
+
+        # Use Pollard's rho for larger factors
         while n > 1:
             factor = pollard_rho(n)
             factors.append(factor)
