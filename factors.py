9d6014053aacc77085432f073aef3a1dd11eab95
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-11-26 12:03:16 -0800

    test

--- factors.py
+++ factors.py
@@ -1,111 +1,48 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 
-import sys
-import random
+check_factor()
+{
+	if [ $# -ne 3 ];
+        then
+                args=("$@")
+                count=0
+                num2=1
+                for a in ${args[*]};
+                do
+                        if [ $count -gt 1 ];
+                        then
+                                num2=$(echo $a*$num2 | bc)
+                        fi
+                        count=$((count + 1))
+                done
+        else
+                num2=$3
+        fi
 
-def gauss(M):
-    marks = [False] * len(M)
-    for j in range(len(M[0])):
-        for i in range(len(M)):
-            if M[i][j] == 1:
-                marks[i] = True
-                for k in range(j):
-                    if M[i][k] == 1:
-                        for row in range(len(M)):
-                            M[row][k] = (M[row][k] + M[row][j]) % 2
-                for k in range(j+1, len(M[0])):
-                    if M[i][k] == 1:
-                        for row in range(len(M)):
-                            M[row][k] = (M[row][k] + M[row][j]) % 2
-                break
-    return (marks, M)
+        num1=$2
+        num=$(echo "$1" | tr ':' '=')
 
-def get_dep_cols(row):
-    ret = []
-    for i in range(len(row)):
-        if row[i] == 1:
-            ret.append(i)
-    return ret
+        result=$(echo "if($num2 > $num1) 1 else 0" | bc)
+	        if ((result == 1)); then
+		        numcp=$num1
+		        num1=$num2
+		        num2=$numcp
+                fi
 
-def row_add(new_row, current):
-    ret = current
-    for i in range(len(M[new_row])):
-        ret[i] ^= M[new_row][i]
-    return ret
+        echo "$num$num1*$num2"
+}
 
-def is_dependent(cols, row):
-    for i in cols:
-        if row[i] == 1:
-            return True
-    return False
+if [ $# -ne 1 ]
+then
+        echo 'Usage: factors <file>'
+        exit 1
+else
 
-def find_linear_deps(row):
-    ret = []
-    dep_cols = get_dep_cols(M[row])
-    current_rows = [row]
-    current_sum = M[row][:]
-    for i in range(len(M)):
-        if i == row:
-            continue
-        if is_dependent(dep_cols, M[i]):
-            current_rows.append(i)
-            current_sum = row_add(i, current_sum)
-        if sum(current_sum) == 0:
-            ret.append(current_rows[:])
-    return ret
+        while read i
+        do
 
-def testdep(dep):
-    x = y = 1
-    for row in dep:
-        x *= smooth_vals[row][0]
-        y *= smooth_vals[row][1]
-    return xgcd(x - isqrt(y), N)[0]
+                result=$(factor "$i")
+                check_factor $result
 
-def phi(p):
-    return p-1
-
-def legendre(a, p):
-    if a % p == 0:
-        return 0
-    return pow(a, (p - 1) // 2, p)
-
-def miller(n, trials=5):
-    s = 0
-    d = n - 1
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    while True:
-        quotient, remainder = divmod(d, 2)
-        if remainder == 1:
-            break
-        s += 1
-        d = quotient
-    for _ in range(trials):
-        a = random.randrange(2, n)
-        composite = True
-        if pow(a, d, n) == 1:
-            continue
-        for i in range(s):
-            if pow(a, 2**i * d, n) == n-1:
-                composite = False
-                break
-        if composite:
-            return False
-    return True
-
-def isqrt(n):
-    x = n
-    y = (x + 1) // 2
-    while y < x:
-        x = y
-        y = (x + n // x) // 2
-    return x
-
-def xgcd(a,b):
-    prevx, x = 1, 0
-    prevy, y = 0, 1
-    while b:
-        q, r = divmod(a,b)
-        x, prevx = prevx - q*x, x
+        done < "$1"
+fi
